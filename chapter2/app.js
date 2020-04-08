2e85ebffc79d223e1c803cb74b51d2c324b10947
reimi <utatatan.013@gmail.com>
2020-04-08 16:00:26 +0900

    [Update] 四則演算/文字列の結合を実行

--- chapter2/app.js
+++ chapter2/app.js
@@ -1,5 +1,18 @@
-var int1 = 1;
-alert(int1);
+// 文字列を結合2
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
+
+// 文字列を結合1
+// alert('Hello' + 'World');
+
+// 足し算を出力
+// alert(4 + 3);
+
+
+// 変数に整数を代入
+// var int1 = 1;
+// alert(int1);
 
 // var hello = 'Hello World'
 // alert(hello);
