23639bab72264d2fd1a876acad2c62cd94500d91
reimi <utatatan.013@gmail.com>
2020-04-09 01:04:29 +0900

    [Update] jQueryでアニメーションを追加する

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,6 +1,20 @@
+// 表示を非表示にする
 $(function(){
-  $('.box1').css({
-  	'background-color': '#0000FF',
-  	'height': '100px'
-  });
+  $('.box1').hide();
 });
+
+// // 非表示を表示する
+// $(function(){
+//    $('.box1').show();
+//    $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// // 下から上にスライド
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
+// // 上から下にスライド
+// $(function(){
+//   $('.box1').slideDown();
+// });
