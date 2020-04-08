cff6279fb0f5f3a0be4058b6ccc7dbd3193be8c7
reimi <utatatan.013@gmail.com>
2020-04-08 17:15:17 +0900

    [Update] 関数を使った表示ができるようにする

---
+++ chapter3/app.js
@@ -0,0 +1,16 @@
+
+// 入力ダイアログで値を入力する
+var promptStr = prompt('何か好きな文字を入力してください。');
+
+alert(promptStr);
+
+// var alertString;
+// alertString = addString("WebCamp");
+
+// alert(alertString);
+
+// // 作成した関数
+// function addString(strA){
+//   var addStr = "Hello " + strA;
+//   return addStr;
+// }
