8ea06dcd71f5672c1c67527a957d576d23666c3c
reimi <utatatan.013@gmail.com>
2020-04-08 16:17:10 +0900

    [Update] 条件分岐/繰り返し処理を実行

--- chapter2/app.js
+++ chapter2/app.js
@@ -1,3 +1,39 @@
+// for文：決められた回数の処理を繰り返す
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+// 	num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+// while文：条件式が真（true）ならば制限なく繰り返す
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// 条件分岐
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple){
+// 	alert('みかんの値段がりんごより安い');
+// }
+// else if(orange == apple){
+// 	alert('みかんとりんごが同じ値段');
+// }
+// else{
+// 	alert('みかんの値段がりんごより高い');
+// }
+
 // 文字列を結合2
 // var str1 = 'Hello';
 // var str2 = 'World!!';
