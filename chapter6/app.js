cca17b355514f52d77be81b1ba9925fc553b6d62
reimi <utatatan.013@gmail.com>
2020-04-09 14:21:10 +0900

    [Update] 「マウスクリック」イベントを設定

--- chapter6/app.js
+++ chapter6/app.js
@@ -1,6 +1,6 @@
-// 「マウスオーバー」「マウスアウト」イベントを追加　書き換え
+//「マウスクリック」イベントを設定
 $(function(){
-   $('.box1').mouseover(function(){
+   $('.box1').on('click', function(){
    	$('.box1').addClass('box1-ext');
    });
 
@@ -8,6 +8,17 @@ $(function(){
    	$('.box1').removeClass('box1-ext');
    });
 });
+
+// 「マウスオーバー」「マウスアウト」イベントを追加　書き換え
+// $(function(){
+//    $('.box1').mouseover(function(){
+//    	$('.box1').addClass('box1-ext');
+//    });
+
+//    $('.box1').mouseout(function(){
+//    	$('.box1').removeClass('box1-ext');
+//    });
+// });
 
 
 // 「マウスオーバー」「マウスアウト」イベントを追加
